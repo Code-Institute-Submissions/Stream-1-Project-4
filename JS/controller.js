7444306fbda1bfd35b48d8f5d1649928d34542a3
Tom <tom_mcnally17@hotmail.co.uk>
2018-03-02 18:21:35 +0000

    updated

--- JS/controller.js
+++ JS/controller.js
@@ -1,2 +1,9 @@
 
+	angular.module("photographerControllers", []);
+
+		app.controller("package-select", function ($scope) {
+
+		$scope.items = ["Please Select", 'Weddings', 'Party', 'Event'];
+		$scope.package = "Please Select";
 	
+		});
