ac01602fa986ef70a4f0c1e9c40c08584cbb8b75
Tom <tom_mcnally17@hotmail.co.uk>
2018-03-04 15:37:04 +0000

    added directives

---
+++ JS/directives.js
@@ -0,0 +1,51 @@
+angular.module("photographerDirectives", [])
+
+	.directive("jumbotron", function() {
+		return {
+			restrict: "EA",
+			templateUrl: "Templates/Directives/jumbotron.html"
+		};
+	})
+
+	.directive("mainNav", function() {
+	    return {
+	        restrict: "EA",
+	        templateUrl: "Templates/Directives/mainNav.html"
+	    };
+	})
+
+	.directive("mainFooter", function() {
+	    return {
+	        restrict: "EA",
+	        templateUrl: "Templates/Directives/mainFooter.html"
+	    };
+	})
+	
+	.directive("formSelect", function() {
+		return {
+			restrict: "EA",
+			templateUrl: "Templates/Directives/formSelect.html"
+		};
+	})
+
+	.directive("formWedding", function() {
+		return {
+			restrict: "EA",
+			templateUrl: "Templates/Directives/formWedding.html"
+		};
+	})
+
+	.directive("formParty", function() {
+		return {
+			restrict: "EA",
+			templateUrl: "Templates/Directives/formParty.html"
+		};
+	})
+
+	.directive("formEvent", function() {
+		return {
+			restrict: "EA",
+			templateUrl: "Templates/Directives/formEvent.html"
+		};
+	});
+
