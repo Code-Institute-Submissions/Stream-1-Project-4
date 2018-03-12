6c41ad4f0a3f33f93934ff7c4dba44aceefc449a
Tom <tom_mcnally17@hotmail.co.uk>
2018-03-12 16:27:06 +0000

    removed AngularJS

--- JS/controller.js
+++
@@ -1,8 +0,0 @@
-angular.module("photographerControllers", [])
-
-	.controller("packageselect", function ($scope) {
-
-		$scope.items = ["Please Select", "Weddings", "Party", "Event"];
-		$scope.package = "Please Select";
-	
-		});
