6a2e4aa21aa89ef78882665dd15c22c01c65a4c1
Tom <tom_mcnally17@hotmail.co.uk>
2018-03-17 17:37:45 +0000

    package form scripts

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,35 @@
 
 	$(document).ready(function() {
-	    $('#introCarousel').carousel({
-	    	
+
+	// Carousel 
+	    $("#introCarousel").carousel({
+	    	interval: 4000    	
 	    });
-	});	
+
+	// Package Planner
+
+		// Package Form Select
+	    $(".event-btn").click(function(event){
+			event.preventDefault();
+
+			var form = $(this).attr("id");
+
+			$(".hidden").css("display", "none")								
+			$("#" + form + "Form").toggle();				
+		});		
+
+		$("#testimonial").submit(function(event){			
+			var testName = $("#testName").val();
+			var testLocation = $("#testLocation").val();
+			var testText = $("#testText").val();
+
+			$("#testMessage").text("Hello " + testName + " from " + testLocation + " if this was real your message would have been delivered");
+			$("#testMessage").css("background-color", "lightgreen");
+		});
+	
+
+
+	});
+
+
+	
